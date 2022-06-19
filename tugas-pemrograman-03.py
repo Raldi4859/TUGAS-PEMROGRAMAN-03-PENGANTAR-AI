85242c62be781c5e8112f1066026faacb8f41587
Rasyid Riyaldi <64974343+Raldi4859@users.noreply.github.com>
2022-06-19 13:56:56 +0700

    Update tugas-pemrograman-03.py

--- tugas-pemrograman-03.py
+++ tugas-pemrograman-03.py
@@ -2,7 +2,7 @@ from openpyxl import load_workbook
 
 ## Load Workbook
 ## Change load_workbook("File Location")
-book = load_workbook("E:/Mata Kuliah/Semester 4/Pengantar Kecerdasan Buatan/Tugas/Tugas Pemrograman 03/traintest.xlsx")
+book = load_workbook("E:/Mata Kuliah/Semester 4/Pengantar Kecerdasan Buatan/Tugas/Tugas Pemrograman 03/TUGAS-PEMROGRAMAN-03-PENGANTAR-AI/traintest.xlsx")
 trainSheet = book['train']
 testSheet = book['test']
 Phi = 3.14259
@@ -125,7 +125,7 @@ def outputToExcel(input):
     for i in range(len(input)):
         testSheet["E"+str(i+2)].value = input[i].y
     ## Change book.save("File Location")
-    book.save("E:/Mata Kuliah/Semester 4/Pengantar Kecerdasan Buatan/Tugas/Tugas Pemrograman 03/traintest.xlsx")
+    book.save("E:/Mata Kuliah/Semester 4/Pengantar Kecerdasan Buatan/Tugas/Tugas Pemrograman 03/TUGAS-PEMROGRAMAN-03-PENGANTAR-AI/traintest.xlsx")
 
 ## Training Session
 print("### Training Session ###")
